5b66edfbfd1efab13504b223b4ebd6c1dc4a5705
Tercus <Terces86@gmail.com>
2017-11-17 00:49:44 +0100

    parsing with files and data from server works
    simplified and standardized the way I handle the data I get. I have a way to parse single VTODO entries which is called each time it is needed. I also changed the tablewisget to not show all the fields available, but only the ones I need. That's because data from the server is going to be stored in the DOM object and not the table.

--- ics_handling.cpp
+++ ics_handling.cpp
@@ -3,43 +3,19 @@
 
 void MainWindow::icsToTable(QString fullText)
 {
-    QString cutBeginning = fullText.mid(fullText.indexOf("BEGIN:VTODO"));
-    QString cleanedText = cutBeginning.left(cutBeginning.lastIndexOf("END:VTODO")+9);
-    QStringList lines = cleanedText.split("\n");
+    QString todoUID = getValueFromIcs(fullText, "UID:");
+    QString todoSummary = getValueFromIcs(fullText, "SUMMARY:");
+    QString todoDescription = getValueFromIcs(fullText, "DESCRIPTION:");
+    QString todoDue = getValueFromIcs(fullText, "DUE:");
+    QString todoDuration = getValueFromIcs(fullText, "DURATION:");
 
-    foreach (QString text, lines) {
-        int x = 0;
-        if (!text.isEmpty())
-        {
-            // Add new row to table for each TODO item in the text
-            if(text == "BEGIN:VTODO") {
-                ui->tableWidget->setRowCount(ui->tableWidget->rowCount()+1);
-            }
-
-            QStringList line = text.split(':');
-
-            // If the table is empty, add first column, loop through existing columns and check if header already exists, add it if not
-            if(ui->tableWidget->columnCount() == 0) {
-                ui->tableWidget->setColumnCount(1);
-                ui->tableWidget->setHorizontalHeaderItem(0, new QTableWidgetItem(line[0]));
-                ui->tableWidget->setItem(ui->tableWidget->rowCount()-1, 0, new QTableWidgetItem(line[1]));
-            }
-            else {
-                while(x < ui->tableWidget->columnCount()) {
-                    if(ui->tableWidget->horizontalHeaderItem(x)->text() == line[0]) {
-                        ui->tableWidget->setItem(ui->tableWidget->rowCount()-1, x, new QTableWidgetItem(line[1]));
-                        break;
-                    }
-                    else if(x == ui->tableWidget->columnCount()-1 && ui->tableWidget->horizontalHeaderItem(x)->text() != line[0]) {
-                        ui->tableWidget->setColumnCount(ui->tableWidget->columnCount()+1);
-                        ui->tableWidget->setHorizontalHeaderItem(x+1, new QTableWidgetItem(line[0]));
-                        ui->tableWidget->setItem(ui->tableWidget->rowCount()-1, x+1, new QTableWidgetItem(line[1]));
-                    }
-                    x++;
-                }
-            }
-        }
-    }
+    int newRow = ui->tableWidget->rowCount();
+    ui->tableWidget->setRowCount(newRow+1);
+    ui->tableWidget->setItem(newRow, 0, new QTableWidgetItem(todoUID));
+    ui->tableWidget->setItem(newRow, 1, new QTableWidgetItem(todoSummary));
+    ui->tableWidget->setItem(newRow, 2, new QTableWidgetItem(todoDescription));
+    ui->tableWidget->setItem(newRow, 3, new QTableWidgetItem(todoDue));
+    ui->tableWidget->setItem(newRow, 4, new QTableWidgetItem(todoDuration));
 }
 
 QString MainWindow::getValueFromIcs(QString fullText, QString searchvalue)
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -36,7 +36,21 @@ void MainWindow::on_actionExit_triggered()
 void MainWindow::on_pushButton_parse_clicked()
 {
     debugMessage("populating the table");
-    icsToTable(ui->plainTextEdit->toPlainText());
+    QString textToParse = ui->plainTextEdit->toPlainText();
+    if(textToParse.count("BEGIN:VTODO") > 1) {
+        QStringList multipleTodo = textToParse.split("END:VTODO");
+        foreach (QString singleTodo, multipleTodo) {
+            if(singleTodo.contains("BEGIN:VTODO")) {
+                icsToTable(singleTodo + "END:VTODO");
+            }
+        }
+    }
+    else if(textToParse.count("BEGIN:VTODO") == 1) {
+        icsToTable(textToParse);
+    }
+    else {
+        debugMessage("The provided Text does not contain any ToDo's. Please check your file.");
+    }
 }
 
 void MainWindow::on_pushButton_openFile_clicked()
@@ -68,31 +82,24 @@ void MainWindow::on_pushButton_beautify_clicked()
 {
     debugMessage("Using data from table to view them nicely");
     ui->listWidget->clear();
-    QStringList tableHeaders;
-    for(int x = 0; x < ui->tableWidget->columnCount(); x++) {
-        tableHeaders << ui->tableWidget->horizontalHeaderItem(x)->text();
-    }
     for (int var = 0; var < ui->tableWidget->rowCount(); var++) {
-        ui->listWidget->addItem(ui->tableWidget->item(var,tableHeaders.indexOf("SUMMARY"))->text());
+        ui->listWidget->addItem(ui->tableWidget->item(var,1)->text());
     }
 }
 
 void MainWindow::onListWidgetlItemClicked(QListWidgetItem*)
 {
-    QStringList tableHeaders;
-    for(int x = 0; x < ui->tableWidget->columnCount(); x++) {
-        tableHeaders << ui->tableWidget->horizontalHeaderItem(x)->text();
-    }
     // unhide the groupbox and show more detailed information (currently gets it from the tablewidget)
     ui->groupBox->setHidden(false);
-    if(ui->tableWidget->item(ui->listWidget->currentRow(), tableHeaders.indexOf("SUMMARY"))) {
-        ui->lineEdit_summary->setText(ui->tableWidget->item(ui->listWidget->currentRow(), tableHeaders.indexOf("SUMMARY"))->text());
+
+    if(ui->tableWidget->item(ui->listWidget->currentRow(), 1)) {
+        ui->lineEdit_summary->setText(ui->tableWidget->item(ui->listWidget->currentRow(), 1)->text());
     }
     else {
         ui->lineEdit_summary->setText("");
     }
-    if(ui->tableWidget->item(ui->listWidget->currentRow(), tableHeaders.indexOf("DESCRIPTION"))) {
-        ui->textEdit_description->setText(ui->tableWidget->item(ui->listWidget->currentRow(), tableHeaders.indexOf("DESCRIPTION"))->text());
+    if(ui->tableWidget->item(ui->listWidget->currentRow(), 2)) {
+        ui->textEdit_description->setText(ui->tableWidget->item(ui->listWidget->currentRow(), 2)->text());
     }
     else {
         ui->textEdit_description->setText("");
